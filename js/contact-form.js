ee694d2081079ffc9e46b6e5e7297a5d8c0ededf
Wojtek Home <voytecu@gmail.com>
2024-01-23 16:57:44 +0100

    some logs for tests

--- js/contact-form.js
+++ js/contact-form.js
@@ -14,14 +14,16 @@ contactForm.addEventListener('submit',(e)=>{
         headers: {'Content-Type':'application/json'},
         body: JSON.stringify(formBody)
     };
-    
+
     fetch('https://urban-web.cyclic.app/urban-web/email/contact',fetchOpt)
         .then(res=>res.json())
 
         .then(ans=>{
-            console.log(ans);
+            console.log("TEST",ans);
             const lng = displayLanguage === 'preferred' ? navigator.language.toUpperCase() : displayLanguage.toUpperCase();
+            console.log("lng",lng);
             runInfoModal(info[lng].contact.title, info[lng].contact.msg);
+            console.log('title',info[lng].contact.title);
         })
 
         .catch(console.log);
